d2267e55584dca11b08e3aa8fb784c1b4fb4762a
Trevor <metatrevor@gmail.com>
2024-04-01 00:00:40 +0300

    Pull images from Pixabay on button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [images, setImages] = useState([])
+    const [searchTerm, setSearchTerm] = useState('moon')
+    const defaultImageCount = 5
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    function pullImages(search) {
+        axios.get(`https://pixabay.com/api/?key=${import.meta.env.VITE_APP_PIXABAY_KEY}&q=${search}&image_type=photo&per_page=${defaultImageCount}`).then(
+            result => {
+                setImages(
+                    result.data.hits
+                )
+                console.log(result.data)
+            }
+        ).catch(error => {
+            console.log(error)
+        })
+    }
+
+    return (
+        <>
+            <h1>Pull Images From Pixabay</h1>
+            <div className="card">
+                <button onClick={() => pullImages(searchTerm)}>
+                    Pull images
+                </button>
+
+                <ul>
+                    {images.map((image) => (
+                        <li key={image.id}> {image.id} - {image.pageURL} </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default App
